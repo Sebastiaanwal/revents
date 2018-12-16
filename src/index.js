fc43ba6c2328fb1f95de1339debdb45074719ade
sebastiaanwal <bramvdwaal@gmail.com>
2018-12-16 12:47:03 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layouts/App';
 import * as serviceWorker from './serviceWorker';
+
+const rootEl = document.getElementById('root')
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
